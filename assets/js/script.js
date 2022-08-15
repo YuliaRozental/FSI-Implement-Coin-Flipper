6b494de47988425f904e8ed8aa1fadf2be2637ec
Yulia <yuyarozental@gmail.com>
2022-08-15 19:58:09 -0400

    coin flipper

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,62 @@
-// TODO: Declare any global variables we need
+// Track any global variables we need
+// All rolls array traks a count for each position. 
+// allRolls = [ones, twos, threes, fours, fives, sixes]
+let allRolls = [0, 0, 0, 0, 0, 0]
 
+// Define a function that updates the scoreboard so that both buttons can reuse it
+let updateScoreboard = function () {
+    // Calculate total number of rolls
+    let total = allRolls[0] + allRolls[1] + allRolls[2] + allRolls[3] + allRolls[4] + allRolls[5]
 
+    // Create a variable to track percentages
+    let allPercents = [0, 0, 0, 0, 0, 0]
+
+    // Before trying to divide, make sure total is not zero
+    if (total > 0) {
+        // Calculate percentages and update scoreboard for each roll
+        for (let i = 0; i < allRolls.length; i++) {
+            // Get the percentage for this roll
+            allPercents[i] = Math.round((allRolls[i] / total) * 100)
+        }
+    }
+
+    // Update the values in the scoreboard table for each value
+    for (let i = 0; i < allRolls.length; i++) {
+        document.getElementById(`td${i + 1}`).textContent = allRolls[i]
+        document.getElementById(`td${i + 1}-percent`).textContent = allPercents[i] + '%'
+    }
+}
+
+// Listen for DOMContentLoaded to ensure that all HTML and resources
+//  have been loaded before attempting to run code
 document.addEventListener('DOMContentLoaded', function () {
-    // This is just a sanity check to make sure your JavaScript script is getting loaded
-    // You can remove it once you see it in your browser console in the developer tools
-    console.log('Hi')
+    // Add event listeners and handlers for both buttons
+    document.getElementById('roll').addEventListener('click', function () {
+        // Determine dice roll using Math.random()
+        let rolledValue = Math.ceil(Math.random() * 6)
 
-    // TODO: Add event listener and handler for flip and clear buttons
+        // Display the result of the roll
+        document.getElementById('message').textContent = `You Rolled ${rolledValue}!`
 
-    // Flip Button Click Handler
-        // TODO: Determine flip outcome
-        // TODO: Update image and status message in the DOM
+        // Add one to the count of number of rolls for the number
+        // Remember arrays are indexed from zero, so it is one less than the rolled value
+        allRolls[rolledValue - 1] += 1
 
-        // Update the scorboard
-        // TODO: Calculate the total number of rolls/flips
-        // Make variables to track the percentages of heads and tails
-        // TODO: Use the calculated total to calculate the percentages
-        // HINT: Make sure not to divide by 0! (if total is 0, percent will be 0 as well)
-        // TODO: Update the display of each table cell
+        // Show different images for each dice roll
+        document.getElementById('dice-image').src = `assets/images/dice/dice${rolledValue}.png`
 
+        // Update the scoreboard
+        updateScoreboard()
+    })
 
-    // Clear Button Click Handler
-        // TODO: Reset global variables to 0
-        // TODO: Update the scoreboard (same logic as in flip button click handler)
+    document.getElementById('clear').addEventListener('click', function () {
+        // Reset the rolls values to zeroes
+        allRolls = [0, 0, 0, 0, 0, 0]
 
+        // Update the message to the user
+        document.getElementById('message').textContent = 'Let\'s Get Rolling!'
+
+        // Update the scoreboard
+        updateScoreboard()
+    })
 })
